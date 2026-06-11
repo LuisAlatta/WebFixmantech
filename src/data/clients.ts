a519c3704c4ec6d0a0374c32ac6262988b1a9267
Luis Alatta <newluisalattago@gmail.com>
2026-06-11 04:14:57 -0500

    mas zoom a los logos de clientes 3-6

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -10,8 +10,8 @@ export interface ClientLogo {
 export const clientLogos: ClientLogo[] = [
     { src: '/nuevas imagenes/CLIENTES/LOGO ANTAMINA.jpeg', alt: 'Antamina' },
     { src: '/nuevas imagenes/CLIENTES/logo CERRO VERDE.jpg', alt: 'Cerro Verde' },
-    { src: '/nuevas imagenes/CLIENTES/logo HUDBAY.jpg', alt: 'Hudbay', scale: 'scale-150' },
-    { src: '/nuevas imagenes/CLIENTES/logo LAS BAMBAS.jpg', alt: 'Las Bambas', scale: 'scale-125' },
-    { src: '/nuevas imagenes/CLIENTES/logo VOLCAN.jpg', alt: 'Volcan', scale: 'scale-150' },
-    { src: '/nuevas imagenes/CLIENTES/logo marcobre.png', alt: 'Marcobre', scale: 'scale-150' }
+    { src: '/nuevas imagenes/CLIENTES/logo HUDBAY.jpg', alt: 'Hudbay', scale: 'scale-[2]' },
+    { src: '/nuevas imagenes/CLIENTES/logo LAS BAMBAS.jpg', alt: 'Las Bambas', scale: 'scale-[1.7]' },
+    { src: '/nuevas imagenes/CLIENTES/logo VOLCAN.jpg', alt: 'Volcan', scale: 'scale-[2]' },
+    { src: '/nuevas imagenes/CLIENTES/logo marcobre.png', alt: 'Marcobre', scale: 'scale-[2]' }
 ];
